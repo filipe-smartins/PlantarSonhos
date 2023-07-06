9d630315b9f201b308259a2f2b896c0bf1e470e2
filipe-smartins <filipe.martins@outlook.com.br>
2023-07-06 13:05:27 -0300

    Ajustes

--- relatorios/views.py
+++ relatorios/views.py
@@ -16,12 +16,14 @@ def relatorios(request):
     total_conta_mei = Transacao.objects.filter(forma_pagamento='CONTA_BANCARIA', conta='MEI').aggregate(total=Sum('valor'))['total']
     total_conta_caixa = Transacao.objects.filter(forma_pagamento='DINHEIRO', conta='CAIXA').aggregate(total=Sum('valor'))['total']
     total_conta_reserva = Transacao.objects.filter(forma_pagamento='RESERVA_CAIXA', conta='RESERVA').aggregate(total=Sum('valor'))['total']
+    total = Transacao.objects.aggregate(total=Sum('valor'))['total']
 
     total_conta_polyana = 0 if total_conta_polyana==None else float(total_conta_polyana)
     total_conta_dayane = 0 if total_conta_dayane == None else float(total_conta_dayane)
     total_conta_mei = 0 if total_conta_mei == None else float(total_conta_mei)
     total_conta_caixa = 0 if total_conta_caixa == None else float(total_conta_caixa)
     total_conta_reserva = 0 if total_conta_reserva == None else float(total_conta_reserva)
+    total = 0 if total == None else float(total)
 
     context = {
         'total_conta_polyana': total_conta_polyana,
@@ -29,6 +31,7 @@ def relatorios(request):
         'total_conta_mei': total_conta_mei,
         'total_conta_caixa': total_conta_caixa,
         'total_conta_reserva': total_conta_reserva,
+        'total': total,
     }
 
     return render(request, 'relatorios.html', context)
